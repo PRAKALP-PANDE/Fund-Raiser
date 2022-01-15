97a8a34406f2ddb91a6426cb4de1f37b6a3b4863
Prakalp <prakalpsp16@gmail.com>
2022-01-15 11:00:31 +0530

    version 1.1.1

---
+++ LInkedListInsertion.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+void LinkedListTraverse(struct Node *ptr)
+{
+    while (ptr != NULL)
+    {
+        /* code */
+        printf("Element of linked list:- %d\n", ptr->data);
+        ptr = ptr->next;
+    }
+}
+
+struct Node *InsertionAtFirst(struct Node *head, int data)
+{
+    struct Node *ptr = (struct NOde *)malloc(sizeof(struct Node));
+    ptr->data = data;
+    ptr->next = head;
+    return ptr;
+};
+
+struct Node *InsertionAtEnd(struct Node *head, int data)
+{
+    struct Node *ptr = (struct Node *)malloc(sizeof(struct Node));
+    ptr->data = data;
+    struct Node *p = head;
+    while (p->next != NULL)
+    {
+        /* code */
+        p = p->next;
+    }
+    p->next = ptr;
+    ptr->next = NULL;
+    return head;
+};
+
+int main()
+{
+
+    struct Node *head;
+    struct Node *second;
+    struct Node *third;
+    struct Node *fourth;
+
+    head = (struct Node *)malloc(sizeof(struct Node));
+    second = (struct Node *)malloc(sizeof(struct Node));
+    third = (struct Node *)malloc(sizeof(struct Node));
+    fourth = (struct Node *)malloc(sizeof(struct Node));
+
+    head->data = 10;
+    head->next = second;
+
+    second->data = 20;
+    second->next = third;
+
+    third->data = 30;
+    third->next = fourth;
+
+    fourth->data = 40;
+    fourth->next = NULL;
+
+    printf("Linked list before any operation\n");
+    LinkedListTraverse(head);
+    printf("After inserting 16 at top of Linked LIst \n");
+    head = InsertionAtFirst(head, 16);
+    LinkedListTraverse(head);
+    printf("Linked list after adding 34 at end\n");
+    head = InsertionAtEnd(head, 34);
+    LinkedListTraverse(head);
+    return 0;
+}
